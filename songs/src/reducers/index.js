afd3bbdd3c6ef7bf0815054d1924d1c878765b56
xionger <xunhao.xiong@gmail.com>
2019-01-05 20:22:52 -0600

    add actions and reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        { title: 'Apologize', duration: '4:10' },
+        { title: 'All Star', duration: '3:20' },
+        { title: 'Crazy', duration: '2:45' },
+        { title: 'Poker Face', duration: '4:05' }
+    ];
+}
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if(action.type === 'SONG_SELECTED'){
+        return action.paylaod;
+    }
+
+    return selectedSong;
+};
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
