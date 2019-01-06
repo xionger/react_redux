550f491a539e61e325b79590b73dc0c19365fa8e
xionger <xunhao.xiong@gmail.com>
2019-01-06 08:03:59 -0600

    init blog app

---
+++ blog/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello, this is the frontpage of Blog App.
+      </div>
+    );
+  }
+}
+
+export default App;
