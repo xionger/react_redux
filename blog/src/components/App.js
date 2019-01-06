7ed8dcdaf59c5aea76f42f99f9aca6eba3fc5c63
xionger <xunhao.xiong@gmail.com>
2019-01-06 08:24:32 -0600

    set up project

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         Hello, this is the frontpage of Blog App.
       </div>
     );
