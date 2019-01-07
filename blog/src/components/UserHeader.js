64926644f9204ffeba35e056e9599a4c2878de78
xionger <xunhao.xiong@gmail.com>
2019-01-06 21:13:27 -0600

    fetch user

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchUser } from '../actions';
 
 class UserHeader extends Component {
+    componentDidMount() {
+        this.props.fetchUser(this.props.userId);
+    }
+    
     render() {
+        const user = this.props.users.find(user => user.id === this.props.userId);
+
+        if(!user){
+            return null;
+        }
+
         return (
-            <div>
-                User Header
-            </div>
+            <div className="header">{ user.name }</div>
         );
     }
 }
 
-export default UserHeader;
+const mapStateToProps = (state) => {
+    return { users: state.users };
+}
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
