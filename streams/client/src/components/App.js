26bd625c5e3cf32a9c2262dee214a3ec51ae54e7
xionger <xunhao.xiong@gmail.com>
2019-01-13 08:24:18 -0600

    use react router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>Page One</div>
+};
+
+const PageTwo = () => {
+    return <div>Page Two</div>
+};
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
     );
 };
 
